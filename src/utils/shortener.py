c11c45246dd85c09c7017c5a8c44e865100c1c25
lohanspies <lohan.spies@didx.co.za>
2021-04-18 07:18:15 +0200

    change URL. need to remove static assignment

--- src/utils/shortener.py
+++ src/utils/shortener.py
@@ -9,5 +9,5 @@ def create_short_url(presentation_request: dict):
     ).decode("utf-8")
     #TODO - fix static variable for url
     # url = f"{settings.SITE_URL}?m={b64_presentation}"
-    url = f"https://localhost:5000?m={b64_presentation}"
+    url = f"https://localhost:8000?m={b64_presentation}"
     return url, b64_presentation
