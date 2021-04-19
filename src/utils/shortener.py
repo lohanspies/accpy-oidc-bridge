6aadfe3dd27bbb61173a4e5ce242baf06c31907d
lohanspies <lohan.spies@didx.co.za>
2021-04-19 17:01:40 +0200

    removed static variable

--- src/utils/shortener.py
+++ src/utils/shortener.py
@@ -1,13 +1,13 @@
 import base64
 import json
-# TODO - import env variables
+import os
 
+SITE_URL = os.getenv('SITE_URL')
 
 def create_short_url(presentation_request: dict):
     b64_presentation = base64.b64encode(
         bytes(json.dumps(presentation_request), "utf-8")
     ).decode("utf-8")
-    #TODO - fix static variable for url
     # url = f"{settings.SITE_URL}?m={b64_presentation}"
-    url = f"https://localhost:8000?m={b64_presentation}"
+    url = f"{SITE_URL}?m={b64_presentation}"
     return url, b64_presentation
